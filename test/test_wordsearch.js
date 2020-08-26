bc10cfae7ac5558a6ba01e2c9156d89f21bbe2d4
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-26 23:16:32 +0000

    Pair programming went well

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,10 +1,9 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
-
+const {wordSearch } = require('../wordsearch.js');
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return false if the word 'FRANK' is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -15,12 +14,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word 'SEINFELD' is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +30,41 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word 'JENNIFER' is present", function() {
+    const result = wordSearch([
+      ['J', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['E', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['N', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['I', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['F', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['R', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'JENNIFER');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word 'SYRYM' is present", function() {
+    const result = wordSearch([
+      ['J', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['E', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['S', 'Y', 'R', 'Y', 'M', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['I', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['F', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['R', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SYRYM');
+
+    assert.isTrue(result);
+  });
+  
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const veritcalCol = transpose(letters);
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (const v of veritcalCol) {
+    if (v.join('').includes(word)) return true;
+  }
+  return false;
 
-module.exports = wordSearch
+};
+
+const transpose = function(matrix) {
+  const copy = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j]) {
+        if (!copy[j]) {
+          copy[j] = [];
+        }
+        copy[j][i] = matrix[i][j];
+      }
+    }
+  }
+  return copy;
+};
+  
+module.exports = {wordSearch};
